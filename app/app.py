241d2effab6d231b2ed65120a91a16eb25fc0e7e
jasonmts <iasonasm@gmail.com>
2018-12-09 03:18:34 +0000

    got database working

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import MySQLdb
+from flask import Response
+from flask import request
 
 
 app = Flask(__name__)
@@ -8,11 +10,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/init")
+@app.route('/init')
 def init():
     try:
-       db = MySQLdb.connect("mysql","root","password")
-       #get access to database
+       db = MySQLdb.connect("localhost", "root", "root")
        cursor = db.cursor()
        cursor.execute("DROP DATABASE IF EXISTS QUOTES")
        cursor.execute("CREATE DATABASE QUOTES")
@@ -26,10 +27,9 @@ def init():
        return "MySQL Error: %s" % str(e)
 
 @app.route("/quotes/add", methods=['POST'])
-
 def add():
     try:
-       db = MySQLdb.connect("mysql","root","password")
+       db = MySQLdb.connect("localhost","root","root")
        cursor = db.cursor()
        cursor.execute("USE QUOTES")
        req_json = request.get_json()
@@ -37,10 +37,43 @@ def add():
              (req_json['Uid'], req_json['Author'], req_json['BookTitle'], req_json['Quote']))
        db.commit()
        return Response("Added\n\n", status=200, mimetype='application/json')
+
     except (MySQLdb.Error, MySQLdb.Warning) as e:
        return "MySQL Error: %s" % str(e)
 
-@app.
+@app.route("/quotes/<Uid>")
+def getquotes():
+    try:
+        db = MySQLdb.connect("localhost","root","root")
+        cursor = db.cursor()
+        cursor.execute("USE QUOTES")
+        cursor.execute("select Author, BookTitle, Quote from courses where ID=" + str(uid))
+        data = cursor.fetchall()
+        if data:
+            return   data + "\n\nSuccess\n\n"
+        else:
+            return "\n\nRecord not found\n\n"
+
+    except (MySQLdb.Error, MySQLdb.Warning) as e:
+        return "MySQL Error: %s" % str(e)
+
+@app.route("/quotes/getall")
+def getquotesall():
+    try:
+        db = MySQLdb.connect("localhost","root","root")
+        cursor = db.cursor()
+        cursor.execute("USE QUOTES")
+        cursor.execute("select * from quotes")
+        data = cursor.fetchall()
+        if data:
+            return json.dumps(data)
+        else:
+            return "\n\nRecord not found\n\n"
+
+    except (MySQLdb.Error, MySQLdb.Warning) as e:
+        return "MySQL Error: %s" % str(e)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # r = requests.post('', data = {'key':'value'})
