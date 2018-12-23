029a2d514c0bb843722b141c2472964d7f21a856
jasonmts <iasonasm@gmail.com>
2018-12-23 22:54:39 +0200

    Add quotes with ajax implemented

--- app/app.py
+++ app/app.py
@@ -18,8 +18,8 @@ def init():
        cursor.execute("DROP DATABASE IF EXISTS QUOTES")
        cursor.execute("CREATE DATABASE QUOTES")
        cursor.execute("USE QUOTES")
-       sql = """CREATE TABLE quotes(Id INT, Author varchar(48),
-                     BookTitle varchar(256), Quote varchar(256));"""
+       sql = """CREATE TABLE quotes(Id INT AUTO_INCREMENT, Author varchar(48),
+                     BookTitle varchar(256), Quote varchar(256), PRIMARY KEY (Id));"""
        cursor.execute(sql)
        db.commit()
        return "\nDB Initialization done\n\n"
@@ -32,30 +32,30 @@ def add():
        db = MySQLdb.connect("db","root","root")
        cursor = db.cursor()
        cursor.execute("USE QUOTES")
-       req_json = request.get_json()
-       cursor.execute("INSERT INTO quotes (Id, Author, BookTitle, Quote) VALUES (%s,%s,%s,%s)",
-             (req_json['Uid'], req_json['Author'], req_json['BookTitle'], req_json['Quote']))
+       # req_json = request.get_json()
+       cursor.execute("INSERT INTO quotes (Author, BookTitle, Quote) VALUES (%s,%s,%s)",
+             (request.form['Author'], request.form['BookTitle'], request.form['Quote']))
        db.commit()
        return Response("Added\n\n", status=200, mimetype='application/json')
 
     except (MySQLdb.Error, MySQLdb.Warning) as e:
        return "MySQL Error: %s" % str(e)
 
-@app.route("/quotes/<Uid>")
-def getquotes():
-    try:
-        db = MySQLdb.connect("db","root","root")
-        cursor = db.cursor()
-        cursor.execute("USE QUOTES")
-        cursor.execute("select Author, BookTitle, Quote from courses where ID=" + str(uid))
-        data = cursor.fetchall()
-        if data:
-            return json.dumps(data)
-        else:
-            return "\n\nRecord not found\n\n"
-
-    except (MySQLdb.Error, MySQLdb.Warning) as e:
-        return "MySQL Error: %s" % str(e)
+# @app.route("/quotes/<Uid>")
+# def getquotes():
+#     try:
+#         db = MySQLdb.connect("db","root","root")
+#         cursor = db.cursor()
+#         cursor.execute("USE QUOTES")
+#         cursor.execute("select Author, BookTitle, Quote from courses where ID=" + str(uid))
+#         data = cursor.fetchall()
+#         if data:
+#             return json.dumps(data)
+#         else:
+#             return "\n\nRecord not found\n\n"
+#
+#     except (MySQLdb.Error, MySQLdb.Warning) as e:
+#         return "MySQL Error: %s" % str(e)
 
 @app.route("/quotes/getall")
 def getquotesall():
---
+++ app/static/index.js
@@ -0,0 +1,20 @@
+function addQuotes()
+{
+  $("#result").html("");
+
+       $.ajax({
+       type: "POST",
+       url: "/quotes/add",
+       dataType: "json",
+       data: $('form').serialize(),
+       success: function(data,status)
+       {
+           alert(data);
+           $("#result").append(data);
+         },
+         complete: function(data,status) {
+            //optional, used for debugging purposes
+             alert(status);
+         }
+ });
+}
