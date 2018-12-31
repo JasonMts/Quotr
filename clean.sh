25eb98c8e3b4d1a7e1320d480164a8ec2c7ec17d
jasonmts <iasonasm@gmail.com>
2018-12-31 17:51:51 +0200

    editted yml scripts and created clean and deploy, app now worksgit add .

---
+++ clean.sh
@@ -0,0 +1,4 @@
+kubectl delete deploy quoteapp
+kubectl delete deploy db
+kubectl delete svc db
+kubectl delete svc quoteapp-svc
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+kubectl apply -f db.yml
+kubectl apply -f db_svc.yml
+kubectl apply -f web.yml
+kubectl apply -f web_svc.yml
